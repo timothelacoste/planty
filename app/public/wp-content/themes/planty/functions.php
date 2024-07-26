27d68410dfabc7b3438271b229c58217d32a278f
Timothe Lacoste <timothe.lacostewp@gmail.com>
2024-07-26 09:05:54 -0600

    feat: Add lazy loading for images and custom logo support

--- app/public/wp-content/themes/planty/functions.php
+++ app/public/wp-content/themes/planty/functions.php
@@ -1,6 +1,13 @@
 <?php
 
-add_action('wp_enqueue_script','theme_enqueue_styles');
-function theme_enqueue_styles() {
+add_action('wp_enqueue_script', 'theme_enqueue_styles');
+function theme_enqueue_styles()
+{
     wp_enqueue_style('parent_style', get_template_directory_uri() . '/style.css');
 }
+
+function themename_custom_logo_setup()
+{
+    add_theme_support('custom-logo');
+}
+add_action('after_setup_theme', 'themename_custom_logo_setup');
