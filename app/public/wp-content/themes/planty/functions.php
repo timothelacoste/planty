f3b538fb9f0822d5b0efb1913b0909f5fba072b4
Timothe Lacoste <timothe.lacostewp@gmail.com>
2024-07-26 08:47:54 -0600

    chore: Add theme files for 'planty' WordPress theme

---
+++ app/public/wp-content/themes/planty/functions.php
@@ -0,0 +1,6 @@
+<?php
+
+add_action('wp_enqueue_script','theme_enqueue_styles');
+function theme_enqueue_styles() {
+    wp_enqueue_style('parent_style', get_template_directory_uri() . '/style.css');
+}
